06c6d46a84633aa4de4f3b20ef8c84eb7467059c
Satya Vamsi Moganti <vamsee1993@gmail.com>
2019-05-30 18:26:06 -0500

    init restful webservices project added

---
+++ restful-webservices-project1/src/main/java/com/resources/rest/webservices/restfulwebservicesproject1/RestfulWebservicesProject1Application.java
@@ -0,0 +1,13 @@
+package com.resources.rest.webservices.restfulwebservicesproject1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestfulWebservicesProject1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestfulWebservicesProject1Application.class, args);
+	}
+
+}
