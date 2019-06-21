8abbe5c14aec0c5e4e5b5a92b9232220a150c99b
Satya Vamsi Moganti <vamsee1993@gmail.com>
2019-06-21 18:29:39 -0500

    include a spring kafka consumer for social network application

---
+++ restful-socialnw-kafkaconsumer/src/main/java/com/resources/rest/webservices/restfulsocialnwkafkaconsumer/RestfulSocialnwKafkaconsumerApplication.java
@@ -0,0 +1,13 @@
+package com.resources.rest.webservices.restfulsocialnwkafkaconsumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestfulSocialnwKafkaconsumerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestfulSocialnwKafkaconsumerApplication.class, args);
+	}
+
+}
---
+++ restful-socialnw-kafkaconsumer/src/main/java/com/resources/rest/webservices/restfulsocialnwkafkaconsumer/constants/Topics.java
@@ -0,0 +1,10 @@
+package com.resources.rest.webservices.restfulsocialnwkafkaconsumer.constants;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Topics {
+
+    public static final String USERS = "users";
+    public static final String POSTS = "posts";
+}
